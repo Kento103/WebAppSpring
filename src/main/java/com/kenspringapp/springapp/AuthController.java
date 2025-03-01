58ffedaad1153f2a5f00edd2bdb9fff889eca1d2
Kento103 <kento103@outlook.com>
2025-03-01 18:48:30 +0900

    認証関連の機能を実装した(Add)
    トップページ、プロフィールページ、管理者向けページを仮作成
    各種ページにログイン状態、権限、非ログイン状態でそれぞれアクセスできるページを仮実装した
    ログインがない場合はログインページに遷移する。

---
+++ src/main/java/com/kenspringapp/springapp/AuthController.java
@@ -0,0 +1,9 @@
+package com.kenspringapp.springapp;
+
+import org.springframework.stereotype.Controller;
+
+// 認証関係のコントローラー
+@Controller
+public class AuthController {
+    // テスト
+}
--- src/main/java/com/kenspringapp/springapp/PageController.java
+++ src/main/java/com/kenspringapp/springapp/PageController.java
@@ -6,9 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class PageController {
+    // ルートディレクトリでトップページを表示する(公開ページ)
     @RequestMapping("/")
     public String topPage() {
         return "こんにちは！HelloWorld!!";
     }
-    
+
+    // ユーザーページを表示する(要認証[一般ユーザ、管理者どちらでも遷移できる])
+    @RequestMapping("/user")
+    public String userPage() {
+        return "ユーザーページへようこそ。";
+    }
+
+    // 管理者向けダッシュボードを表示する(要認証[管理者のみ表示が許可される])
+    @RequestMapping("/admin")
+    public String adminPage() {
+        return "管理者向けダッシュボードへようこそ。";
+    }
 }
